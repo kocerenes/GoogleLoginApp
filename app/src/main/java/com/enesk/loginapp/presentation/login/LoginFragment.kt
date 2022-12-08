149e164714603ea31cc292941615a0aaef8d77d2
kocerenes <eneskocerr41@gmail.com>
2022-12-08 22:18:50 +0300

    Code refactoring

--- app/src/main/java/com/enesk/loginapp/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/enesk/loginapp/presentation/login/LoginFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.enesk.loginapp.R
 import com.enesk.loginapp.databinding.FragmentLoginBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -15,14 +17,15 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 
-
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var mGoogleSignInClient: GoogleSignInClient
-    private val RC_SIGN_IN = 9001
+    private val SIGN_IN = 9001
+
+    //private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,8 +34,18 @@ class LoginFragment : Fragment() {
             .requestEmail()
             .build()
 
-        // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = context?.let { GoogleSignIn.getClient(it, gso) }!!
+
+        // We can do the assignment inside onAttach or onCreate
+        /*activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RC_SIGN_IN) {
+                val intent = result.data
+                val task =
+                    GoogleSignIn.getSignedInAccountFromIntent(intent)
+                handleSignInResult(task)
+                Navigation.findNavController(binding.root).navigate(R.id.action_loginFragment_to_homeFragment)
+            }
+        }*/
     }
 
     override fun onCreateView(
@@ -43,8 +56,6 @@ class LoginFragment : Fragment() {
 
         binding.ivGoogle.setOnClickListener {
             signIn()
-
-
         }
 
         return binding.root
@@ -52,44 +63,37 @@ class LoginFragment : Fragment() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == RC_SIGN_IN) {
+        if (requestCode == SIGN_IN) {
             val task =
                 GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleSignInResult(task)
+            handleResult(task)
+            Navigation.findNavController(binding.root).navigate(R.id.action_loginFragment_to_homeFragment)
         }
     }
 
-    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>) {
         try {
             val account = completedTask.getResult(
                 ApiException::class.java
             )
-            // Signed in successfully
             val googleId = account?.id ?: ""
-            Log.i("Google ID",googleId)
+            Log.i("Google ID", googleId)
             val googleFirstName = account?.givenName ?: ""
             Log.i("Google First Name", googleFirstName)
             val googleLastName = account?.familyName ?: ""
             Log.i("Google Last Name", googleLastName)
             val googleEmail = account?.email ?: ""
             Log.i("Google Email", googleEmail)
-            val googleProfilePicURL = account?.photoUrl.toString()
-            Log.i("Google Profile Pic URL", googleProfilePicURL)
-            val googleIdToken = account?.idToken ?: ""
-            Log.i("Google ID Token", googleIdToken)
         } catch (e: ApiException) {
-            // Sign in was unsuccessful
-            Log.e(
-                "failed code=", e.statusCode.toString()
-            )
+            Log.e("failed code=", e.statusCode.toString())
         }
     }
 
     private fun signIn() {
         val signInIntent = mGoogleSignInClient.signInIntent
         startActivityForResult(
-            signInIntent, RC_SIGN_IN
+            signInIntent, SIGN_IN
         )
+        //activityResultLauncher.launch(signInIntent)
     }
-
 }
